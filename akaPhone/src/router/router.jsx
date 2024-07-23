919a20b2a80dcba9bce17a80a6c8522ad9f9a6c0
Rustamjonov Davron <davronalirustamjonov@gmail.com>
2024-07-23 17:45:21 +0500

    Add

--- akaPhone/src/router/router.jsx
+++ akaPhone/src/router/router.jsx
@@ -1,9 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Nested from "../components/Nested";
-import Lavash from "../components/Lavash";
-import Desert from "../components/Desert";
-import Set from "../components/Set";
 import Slider from "../Components/Slider";
 
 const routes = [
@@ -16,18 +12,7 @@ const routes = [
         path: "/",
         element: <Slider/>,
         children: [
-          {
-            path: "/",
-            element: </>,
-          },
-          {
-            path: "/",
-            element: < />,
-          },
-          {
-            path: "/",
-            element: < />,
-          },
+         
         ],
       },
     ],
